459aa2eb64cb8259feeab74b60e84fd245b5c3f6
Aleksey Z <aleksey.rapcat@gmail.com>
2020-12-25 15:05:29 +0300

    add: HW for the lesson seven

---
+++ src/lesson_seven/Cat.java
@@ -0,0 +1,30 @@
+package lesson_seven;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    public void eat(Plate p) {
+       setSatiety(p.decreaseFood(appetite));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    public boolean getSatiety(){
+        return satiety;
+    }
+}
